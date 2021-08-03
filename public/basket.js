b96d33c48cb70393668d9b8497355ec01398a1fa
Jonathan Gil <jonathan.gil066@gmail.com>
2021-08-03 09:33:51 +0200

    modification pour le prix total

--- public/basket.js
+++ public/basket.js
@@ -2,6 +2,7 @@ const insertTableau = document.getElementById('tableau');
 const articles = JSON.parse(localStorage.getItem("articles"));
 console.log(articles);
 let priceT ;
+var quantite = 0;
 const fetchingTeddies = () =>{
     articles.forEach(async article =>{
         const response = (await fetch("http://localhost:3000/api/teddies/" + article.id));
@@ -12,23 +13,25 @@ const fetchingTeddies = () =>{
         insertTableau.appendChild(id);
     });
 }
+
 const initBasket = () =>{
     const title = document.createElement("caption");
     title.innerHTML = "Récapitulatif de votre commande :" ;
     insertTableau.appendChild(title);
 }
-
-const footBasket = () =>{
+const footBasket = () => {
+    articles.forEach(async article => {
+        const response = (await fetch("http://localhost:3000/api/teddies/" + article.id));
+        const teddie = await response.json();
+        priceT = teddie.price * article.quantity;
+        quantite += priceT;
+        console.log(quantite);
+    });
     const foot = document.createElement("tfoot");
-    for (let i = 0; i < articles.length ; i++) {
-    let quantite =+ articles.quantity;
-    return quantite;
-    }
-    console.log(quantitee);
-    foot.innerHTML = '' ;
+    foot.innerHTML = `<td>Prix total : ${quantite/100}</td>` ;
     insertTableau.appendChild(foot);
-};
+}
 
 initBasket();
-fetchingTeddies();
 footBasket();
+fetchingTeddies();
