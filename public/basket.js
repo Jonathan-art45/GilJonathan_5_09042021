5653cc62c08df22523643bfe0c40baf3fd591c9a
Jonathan Gil <jonathan.gil066@gmail.com>
2021-08-04 12:06:50 +0200

    modification pour le prix total mise en place du bouton supprimer

--- public/basket.js
+++ public/basket.js
@@ -1,37 +1,43 @@
-const insertTableau = document.getElementById('tableau');
 const articles = JSON.parse(localStorage.getItem("articles"));
 console.log(articles);
-let priceT ;
-var quantite = 0;
-const fetchingTeddies = () =>{
-    articles.forEach(async article =>{
-        const response = (await fetch("http://localhost:3000/api/teddies/" + article.id));
-        const teddie = await response.json();
-        priceT = teddie.price * article.quantity;
-        const id = document.createElement("tr");
-        id.innerHTML = `<td>${teddie.name}</td><td>${teddie.price/100}€</td><td>${article.quantity}</td><td>${priceT/100}€</td>`;
-        insertTableau.appendChild(id);
-    });
-}
+if (articles === null){
+    document.getElementById("basketNull").innerHTML = `<p>Votre panier est vide.</p>`;
+    let supp = document.getElementById("tableau");
+    supp.style.display = "none";
+}else {
+    const insertTableau = document.getElementById('tableau');
+    let priceT;
+    let priceTotalFinal = 0;
+    const fetchingTeddies = () => {
+        var quantitePriceTotal = 0;
+        articles.forEach(async article => {
+            const response = (await fetch("http://localhost:3000/api/teddies/" + article.id));
+            const teddie = await response.json();
+            priceT = teddie.price * article.quantity;
+            const id = document.createElement("tr");
+            id.innerHTML = `<td>${teddie.name}</td><td>${teddie.price / 100}€</td><td>${article.quantity}</td><td>${priceT / 100}€</td><td class="center"><i class="fas fa-times"></i></td>`;
+            insertTableau.appendChild(id);
+            priceTotalFinal += priceT / 100;
+            foot.innerHTML = `<td>Prix total : ${priceTotalFinal} €</td>`;
+        });
+    }
 
-const initBasket = () =>{
-    const title = document.createElement("caption");
-    title.innerHTML = "Récapitulatif de votre commande :" ;
-    insertTableau.appendChild(title);
-}
-const footBasket = () => {
-    articles.forEach(async article => {
-        const response = (await fetch("http://localhost:3000/api/teddies/" + article.id));
-        const teddie = await response.json();
-        priceT = teddie.price * article.quantity;
-        quantite += priceT;
-        console.log(quantite);
-    });
+    const initBasket = () => {
+        const title = document.createElement("caption");
+        title.innerHTML = "Récapitulatif de votre commande :";
+        title.style.marginBottom = "1rem";
+        insertTableau.appendChild(title);
+    }
+
     const foot = document.createElement("tfoot");
-    foot.innerHTML = `<td>Prix total : ${quantite/100}</td>` ;
     insertTableau.appendChild(foot);
-}
 
-initBasket();
-footBasket();
-fetchingTeddies();
+    initBasket();
+    fetchingTeddies();
+
+
+    const removeBasket = () => {
+
+    }
+    removeBasket();
+}
