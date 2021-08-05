425599454c466fed127921a9bb319e9bc9dd2f7d
Jonathan Gil <jonathan.gil066@gmail.com>
2021-08-05 18:49:47 +0200

    mise en place du boutton supprimer

--- public/basket.js
+++ public/basket.js
@@ -1,10 +1,13 @@
-const articles = JSON.parse(localStorage.getItem("articles"));
+let articles = JSON.parse(localStorage.getItem("articles"));
 console.log(articles);
-if (articles === null){
+
+if (articles === null || articles == 0){
     document.getElementById("basketNull").innerHTML = `<p>Votre panier est vide.</p>`;
     let supp = document.getElementById("tableau");
     supp.style.display = "none";
+
 }else {
+    document.getElementById("basketNull").style.display = "none";
     const insertTableau = document.getElementById('tableau');
     let priceT;
     let priceTotalFinal = 0;
@@ -15,7 +18,7 @@ if (articles === null){
             const teddie = await response.json();
             priceT = teddie.price * article.quantity;
             const id = document.createElement("tr");
-            id.innerHTML = `<td>${teddie.name}</td><td>${teddie.price / 100}€</td><td>${article.quantity}</td><td>${priceT / 100}€</td><td class="center"><i class="fas fa-times"></i></td>`;
+            id.innerHTML = `<td>${teddie.name}</td><td>${teddie.price / 100}€</td><td>${article.quantity}</td><td>${priceT / 100}€</td><td class="center"><button class="removeArticle" type="button"><i class="fas fa-times "></i></button></td>`;
             insertTableau.appendChild(id);
             priceTotalFinal += priceT / 100;
             foot.innerHTML = `<td>Prix total : ${priceTotalFinal} €</td>`;
@@ -32,12 +35,33 @@ if (articles === null){
     const foot = document.createElement("tfoot");
     insertTableau.appendChild(foot);
 
+    const removeArt = () => {
+
+        let removeArticle = document.getElementsByClassName("removeArticle");
+        console.log(removeArticle);
+        for (let i = 0; i < removeArticle.length; i++) {
+            removeArticle[i].addEventListener("click", (e) => {
+                let removeId = articles[i].id;
+                console.log(removeId);
+                articles = articles.filter(function (article){
+                    if (article.id !== removeId)
+                    return true
+                    console.log(articles)
+                });
+                fetchingTeddies();
+                console.log(articles);
+                localStorage.setItem("articles", JSON.stringify(articles));
+            })
+        }
+    }
+
     initBasket();
     fetchingTeddies();
-
-
-    const removeBasket = () => {
-
-    }
-    removeBasket();
+    setTimeout(function(){
+        removeArt()
+    },1000);
 }
+
+
+//let articlesFilters = articles.filter(function (){})
+
